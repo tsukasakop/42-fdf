fff467ee9c7dd73fb6d68248726eb4fbd7cb800b
Tsukasa Kondo <tsukasakop@MacBook-Air-2018.local>
2024-12-28 19:26:25 +0900

    add header

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 10:22:23 by tkondo            #+#    #+#             */
-/*   Updated: 2024/12/26 10:23:00 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/12/27 01:38:13 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 # define FDF_H
 
 # ifndef CANVAS_W
-#  define CANVAS_W 1920
+#  define CANVAS_W 2560
 # endif
 # ifndef CANVAS_H
-#  define CANVAS_H 1080
+#  define CANVAS_H 1600
 # endif
 
 # include <stdint.h>
+#include <stddef.h>
 
 typedef uint64_t		t_color;
 typedef struct s_model	t_model;
 typedef struct s_point	t_point;
 typedef struct s_image	t_image;
 typedef struct s_pixel	t_pixel;
+
+typedef struct s_norm	t_norm;
+
+struct s_norm{
+	int		max[2];
+	int		min[2];
+	double scale[2];
+	int		move[2];
+};
 
 struct					s_image
 {
@@ -56,16 +66,16 @@ struct					s_point
 struct					s_model
 {
 	t_point				p[1000][1000];
-	int					z_min;
-	int					z_max;
-	int					y_max;
-	int					x_max;
+	int					min_z;
+	int					max_z;
+	size_t					x_len;
+	size_t					y_len;
 };
 
 struct					s_angle
 {
 };
-
+t_model	*get_model(char *fname);
 /*
 model					read_model(file);
 camera					camera_new(void);
